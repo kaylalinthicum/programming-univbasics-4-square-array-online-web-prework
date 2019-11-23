f9122ba6f854a89b45488b607eed556361d98d1d
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-23 02:37:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers)
   counter = 0 
+  
   while counter < numbers.length do 
-  puts numbers[counter]**2
-  counter += 1
+    puts numbers[counter]**2
+    counter += 1
   end
 end
